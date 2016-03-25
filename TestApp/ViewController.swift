539195c35967a3db4f9b8fb35a245206e6f7921f
Kamil Wysocki <kamil.wysocki@brightinventions.pl>
2016-03-25 13:47:37 +0100

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Kamil Wysocki on 25/03/16.
+//  Copyright © 2016 Kamil Wysocki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
