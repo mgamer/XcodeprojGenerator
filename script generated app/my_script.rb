e6a40fbb4082824a2bb0aa065a6d4056ac212c29
Kamil Wysocki <kamil.wysocki@brightinventions.pl>
2016-04-01 11:04:05 +0200

    Add bundle identifier name to Identity

--- script generated app/my_script.rb
+++ script generated app/my_script.rb
@@ -24,7 +24,6 @@ module Xcodeproj
 					end
 				end
 			end
-
 		end
 
 
@@ -53,14 +52,45 @@ module Xcodeproj
 				'GCC_PREPROCESSOR_DEFINITIONS' => ["DEBUG=1", "$(inherited)"],
 				'GCC_SYMBOLS_PRIVATE_EXTERN' => 'NO',
 				'GCC_OPTIMIZATION_LEVEL' => '0',
-			    'INFOPLIST_FILE' => 'src/Info.plist'
+			    'INFOPLIST_FILE' => 'src/Info.plist',
+			    'PRODUCT_BUNDLE_IDENTIFIER' => 'pl.bright.inventions.$(PRODUCT_NAME)'
 			}
+
+			buildSettingsConfigRelease = {
+				'ALWAYS_SEARCH_USER_PATHS' => 'NO',
+				'GCC_C_LANGUAGE_STANDARD' => 'gnu99',
+				'INSTALL_PATH' => "$(BUILT_PRODUCTS_DIR)",
+				'GCC_WARN_ABOUT_MISSING_PROTOTYPES' => 'YES',
+				'GCC_WARN_ABOUT_RETURN_TYPE' => 'YES',
+				'GCC_WARN_UNUSED_VARIABLE' => 'YES',
+				'OTHER_LDFLAGS' => '',
+				'ARCHS' => "$(ARCHS_STANDARD_32_BIT)",
+				'IPHONEOS_DEPLOYMENT_TARGET' => '9.2',
+				'PUBLIC_HEADERS_FOLDER_PATH' => "$(TARGET_NAME)",
+				'SDKROOT' => 'iphoneos',
+				'GCC_DYNAMIC_NO_PIC' => 'NO',
+				'GCC_PREPROCESSOR_DEFINITIONS' => ["DEBUG=1", "$(inherited)"],
+				'GCC_SYMBOLS_PRIVATE_EXTERN' => 'NO',
+				'GCC_OPTIMIZATION_LEVEL' => '0',
+			    'INFOPLIST_FILE' => 'src/Info.plist',
+			    'PRODUCT_BUNDLE_IDENTIFIER' => 'pl.bright.inventions.$(PRODUCT_NAME)'
+			}
+
+
+			project.objects.each do |value|
+				puts value
+			end
 			
 			buildSettingsConfig.each do |key, array|
-				print "#{key}=>"
-				puts array
+				# print "#{key}=>"
+				# puts array
 				project.objects[3].set_setting(key,array) #fix it 
 			end
+
+			# buildSettingsConfigRelease.each do |key, array|
+			# 	project.objects[5].set_setting(key,array)
+			# end
+
 
 			# puts project.build_settings("Release")
 			# project.objects.each { |value | puts value }
