cac79e5fed492fcd7c0b92f623055af67bfd915b
Kamil Wysocki <kamil.wysocki@brightinventions.pl>
2016-04-01 12:36:26 +0200

    Get app icons and .xcassets catalog

--- Example/my_script.rb
+++ Example/my_script.rb
@@ -1,6 +1,12 @@
+# TODO : 
+# 1.Settings bundle
+# 2.Build settings for different configurations
+
+
+
+
 require 'rubygems'
 require 'xcodeproj'
-
 
 
 module Xcodeproj
@@ -14,13 +20,15 @@ module Xcodeproj
 
 		def self.add_files_to_group(currentGroup, path, applicationTarget)
 			Dir.glob(path + '/*') do |file|
-				if File.directory?(file) 
+				if File.directory?(file) and !(file.include? ".xcassets")
 					newGroup = Xcodeproj::Project.add_new_group(currentGroup, file)
 					Xcodeproj::Project.add_files_to_group(newGroup, file, applicationTarget)
 				else
 					ref = currentGroup.new_reference(file)
 					if file.include? ".swift"
 						applicationTarget.source_build_phase.add_file_reference(ref)
+					elsif file.include? ".xcassets"
+						applicationTarget.resources_build_phase.add_file_reference(ref)
 					end
 				end
 			end
@@ -53,44 +61,14 @@ module Xcodeproj
 				'GCC_SYMBOLS_PRIVATE_EXTERN' => 'NO',
 				'GCC_OPTIMIZATION_LEVEL' => '0',
 			    'INFOPLIST_FILE' => 'src/Info.plist',
-			    'PRODUCT_BUNDLE_IDENTIFIER' => 'pl.bright.inventions.$(PRODUCT_NAME)'
+			    'PRODUCT_BUNDLE_IDENTIFIER' => 'pl.brightinventions.$(PRODUCT_NAME)'
 			}
-
-			buildSettingsConfigRelease = {
-				'ALWAYS_SEARCH_USER_PATHS' => 'NO',
-				'GCC_C_LANGUAGE_STANDARD' => 'gnu99',
-				'INSTALL_PATH' => "$(BUILT_PRODUCTS_DIR)",
-				'GCC_WARN_ABOUT_MISSING_PROTOTYPES' => 'YES',
-				'GCC_WARN_ABOUT_RETURN_TYPE' => 'YES',
-				'GCC_WARN_UNUSED_VARIABLE' => 'YES',
-				'OTHER_LDFLAGS' => '',
-				'ARCHS' => "$(ARCHS_STANDARD_32_BIT)",
-				'IPHONEOS_DEPLOYMENT_TARGET' => '9.2',
-				'PUBLIC_HEADERS_FOLDER_PATH' => "$(TARGET_NAME)",
-				'SDKROOT' => 'iphoneos',
-				'GCC_DYNAMIC_NO_PIC' => 'NO',
-				'GCC_PREPROCESSOR_DEFINITIONS' => ["DEBUG=1", "$(inherited)"],
-				'GCC_SYMBOLS_PRIVATE_EXTERN' => 'NO',
-				'GCC_OPTIMIZATION_LEVEL' => '0',
-			    'INFOPLIST_FILE' => 'src/Info.plist',
-			    'PRODUCT_BUNDLE_IDENTIFIER' => 'pl.bright.inventions.$(PRODUCT_NAME)'
-			}
-
-
-			project.objects.each do |value|
-				puts value
-			end
 			
 			buildSettingsConfig.each do |key, array|
-				# print "#{key}=>"
-				# puts array
+				print "#{key}=>"
+				puts array
 				project.objects[3].set_setting(key,array) #fix it 
 			end
-
-			# buildSettingsConfigRelease.each do |key, array|
-			# 	project.objects[5].set_setting(key,array)
-			# end
-
 
 			# puts project.build_settings("Release")
 			# project.objects.each { |value | puts value }
